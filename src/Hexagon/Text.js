075f4e9b93610a4c3f3380b1fabf29690fcbb068
Jason Zeederberg <jason@assemble.co.za>
2019-06-21 12:17:50 +0200

    Added props to text

--- src/Hexagon/Text.js
+++ src/Hexagon/Text.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 // TODO Text is a separate component so that it could wrap the given text inside the surrounding hexagon
 class Text extends Component {
   static propTypes = {
     children: PropTypes.string,
-    x: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number
-    ]),
-    y: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number
-    ]),
-    className: PropTypes.string
-  };
+    x: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    y: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    className: PropTypes.string,
+    height: PropTypes.number,
+    width: PropTypes.number,
+    textAnchor: PropTypes.string
+  }
 
   render() {
-    const { children, x, y, className } = this.props;
+    const { children, x, y, className, textAnchor, height, width } = this.props
     return (
-      <foreignObject x={x || 0} y={y ? y : '0.3em'} className={className} textAnchor="middle">{children}</foreignObject>
-    );
+      <foreignObject
+        x={x ? x : '0.3em'}
+        y={y ? y : '0.3em'}
+        className={className}
+        textAnchor={textAnchor}
+        height={height}
+        width={width}>
+        {children}
+      </foreignObject>
+    )
   }
 }
 
-export default Text;
+export default Text
